ccd48ded97009db0434034fe74998fd7f0ce9a6e
AbelCourse <96333087+AbelCourse@users.noreply.github.com>
2024-04-18 14:17:22 -0500

    Revert "Lab11 setup"
    This reverts commit f0eb5eadd1f1d4f59a38c94d049dcde2fdc60c4.

--- Lab11/src/main/java/edu/miu/cs/cs489/lesson6/citylibraryapp/dao/AppointementRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.miu.cs.cs489.lesson6.citylibraryapp.dao;
-
-import edu.miu.cs.cs489.lesson6.citylibraryapp.models.Appointment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppointementRepository extends JpaRepository<Appointment, Long> {
-}
--- Lab11/src/main/java/edu/miu/cs/cs489/lesson6/citylibraryapp/service/ISurgeryService.java
+++
@@ -1,7 +0,0 @@
-package edu.miu.cs.cs489.lesson6.citylibraryapp.service;
-
-import edu.miu.cs.cs489.lesson6.citylibraryapp.models.Surgery;
-
-public interface ISurgeryService {
-    Surgery save(Surgery surgery);
-}
